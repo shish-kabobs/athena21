1c6beb8977cf32eb126aa7f08dc5d0bf42d7b856
Kate Bobyn <kbobyn1@sheffield.ac.uk>
2021-04-18 11:49:01 +0100

    ability to view status for users, ability to accept or reject for charity shop

--- controllers/donation.js
+++ controllers/donation.js
@@ -9,3 +9,35 @@ exports.getDonations = (req, res) => {
         res.render('donations', { donations: docs });
     });
 };
+
+/**
+ * POST /donations
+ * List all pending donations.
+ */
+exports.updateDonationStatus = (req, res) => {
+    Donation.findById(req.body._id, (err, donation) => {
+        if (err) { return next(err); }
+        donation.status = "Accepted";
+        // todo: also edit the user's karma
+        donation.save((err) => {            
+            req.flash('success', { msg: 'User will be notified you would appreciate their donation.' });
+            res.redirect('/donations');
+        });
+    });
+}
+
+/**
+ * POST /donations
+ * List all pending donations.
+ */
+exports.rejectDonation = (req, res) => {
+    Donation.findById(req.body._id, (err, donation) => {
+        if (err) { return next(err); }
+        donation.status = "Rejected";
+        // also edit the user's karma
+        donation.save((err) => {
+            req.flash('success', { msg: 'User will be notified.' });
+            res.redirect('/donations');
+        });
+    });
+}
